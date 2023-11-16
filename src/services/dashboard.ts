45b357fbd115ff46a6046e3e403f4f8502fd0013
kose-uk45 <kosebilaliuk45@gmail.com>
2023-11-16 22:41:06 +0200

    fix(auth):add fecth basic authentication for non public dashboards

--- src/services/dashboard.ts
+++ src/services/dashboard.ts
@@ -1,9 +1,19 @@
 import { API_BASE_URL } from '../config/api';
 import { Dashboard } from '../types/dashboards';
 // Below functions Implement fetch api to get api data
-
+const auth = {
+  method: 'GET',
+  headers: new Headers({
+    Authorization:
+      'Basic ' +
+      btoa(
+        `${process.env.REACT_APP_USERNAME}:${process.env.REACT_APP_PASSWORD}`
+      ),
+    'Content-Type': 'application/json',
+  }),
+};
 const fetchDashboards = async () => {
-  const response = await fetch(`${API_BASE_URL}/dashboards.json`);
+  const response = await fetch(`${API_BASE_URL}`, auth);
   if (!response.ok) {
     throw new Error(
       'Failed to fetch dashboards. HTTP status: ' + response.status
@@ -14,7 +24,7 @@ const fetchDashboards = async () => {
 };
 
 const fetchDashboardItems = async (id: string) => {
-  const response = await fetch(`${API_BASE_URL}/${id}.json`);
+  const response = await fetch(`${API_BASE_URL}/${id}.json`, auth);
   if (!response.ok) {
     throw new Error(
       'Failed to fetch dashboards. HTTP status: ' + response.status
